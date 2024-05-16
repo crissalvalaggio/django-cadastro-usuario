b7542a549a75a97faa81b73b46c5be5549e891d1
Cristian Salvalaggio <99545448+crissalvalaggio@users.noreply.github.com>
2024-05-16 15:01:51 -0300

    att v3

--- projeto_cad_usuario/app/views.py
+++ projeto_cad_usuario/app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import usuario
 
 # Create your views here.
 
@@ -6,3 +7,15 @@ from django.shortcuts import render
 def home(request):
     return render(request, 'usuarios/home.html')
 
+def usuarios(request):
+    #salvar os dados da tela para o banco de dados
+    novo_usuario = usuario()
+    novo_usuario.nome = request.POST.get('nome')
+    novo_usuario.idade = request.POST.get('idade')
+    novo_usuario.save()
+    # exibir todos em uma nova página
+    usuarios = {
+        'usuarios': usuario.objetcs.all()
+    }
+
+    return render(request,'usuarios/usuarios.html', usuarios)
